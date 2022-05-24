0529a616c6c818180633b07f1ec351b08f96954f
AmberAAA <anborong@outlook.com>
2022-05-24 11:02:11 +0800

    迭代最新版本到v2.0

---
+++ readline.zsh
@@ -0,0 +1,5 @@
+#!/bin/zsh
+while read line
+do
+    echo $line
+done < $1
--- start.sh
+++ start.sh
@@ -1,27 +1,49 @@
 #!/bin/zsh
 
-echo "一键上班v1.0"
+cloudApp(){
+    echo "启动cloudApp"
+    cd ~/code/cloudApp && npm run serve  > /dev/null 2>&1 &
+}
+
+cloudStandard(){
+    echo "启动cloudStard"
+    cd ~/code/cloudStandard && npm run serve  > /dev/null 2>&1 &
+}
+
+demon(){
+    echo "启动传媒版"
+    cd ~/code/demon && npm run start  >  /dev/null 2>&1 &
+}
+
+media_editor(){
+    echo "启动媒体编辑器"
+    cd ~/code/media_editor && npm run serve  > /dev/null 2>&1 &
+}
+
+echo "一键上班v1.1"
 
 echo "先下班 不破不立"
 stop
 
 # 一键上班
+if [ -z "$1" ]; then
+    cloudApp
+    cloudStandard
+    demon
+    media_editor
+else
+    for i in $@
+    do
+        if [[ $i == "cloudApp" || $i == "ca" ]]; then
+            cloudApp
+        elif [[ $i == "cloudStandard" || $i == "cs" ]]; then
+            cloudStandard
+        elif [[ $i == "demon" || $i == "d" ]]; then
+            demon
+        elif [[ $i == "media_editor" || $i == "m" ]]; then
+            media_editor
+        fi
+    done
+fi
 
-echo "" > ~/log
-
-echo "启动cloudApp"
-cd ~/code/cloudApp
-npm run serve  >> ~/log 2>&1 &
-
-echo "启动cloudStard"
-cd ~/code/cloudStandard
-npm run serve  >> ~/log 2>&1 &
-
-echo "启动传媒版"
-cd ~/code/demon
-npm run start  >> ~/log 2>&1 &
-
-echo "启动媒体编辑器"
-cd ~/code/media_editor
-npm run serve  >> ~/log 2>&1 &
 
