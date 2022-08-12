d2a5d3b5c32a0444ee8f3d94bf889b331b9102f8
AmberAAA <anborong@outlook.com>
2022-08-12 09:03:24 +0800

    update refress

--- refresh.sh
+++ refresh.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 time=3
-for i in {1..3}
+for i in {1..10}
 do
-  nslookup srjc.anborong.top >> /dev/null
+  nslookup srjc.anborong.top | awk 'NR==6{print $2}' | xargs echo "srjc.anborong.top:"
 done
