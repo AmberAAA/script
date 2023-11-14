c42711c139bc4a5127b72a082ca21a386983e4d5
AmberAAA <anborong@outlook.com>
2023-11-14 09:32:14 +0800

    update proxy

--- proxy_swith.sh
+++ proxy_swith.sh
@@ -3,31 +3,31 @@
 echo "切换命令行代理 V1.0"
 
 
-http_proxy="http://127.0.0.1:7890"
-socks5_proxy="socks5://127.0.0.1:7890"
+http_proxy="http://192.168.1.3:20171"
+socks5_proxy="socks5://192.168.1.3:20170"
 
 flag=0
 
 setProxy(){
     echo "配置代理中..."
-    export "http_proxy=$http_proxy"
-    export "https_proxy=$http_proxy"
+    export "HTTP_PROXY=$http_proxy"
+    export "HTTPS_PROXY=$http_proxy"
     export "ALL_PROXY=$socks5_proxy"
     npm config set proxy $http_proxy
-    echo "http_proxy: $http_proxy"
-    echo "socks5_proxy: $socks5_proxy"
+    echo "HTTP_PROXY: $http_proxy"
+    echo "SOCKS5: $socks5_proxy"
 }
 
 
 unsetProxy() {
     echo "取消代理"
-    unset http_proxy
-    unset https_proxy
+    unset HTTP_PROXY
+    unset HTTPS_PROXY
     unset ALL_PROXY
     npm config delete proxy
 }
 
-for i in $(env | grep proxy)
+for i in $(env | grep PROXY)
 do
     echo $i
     flag=1
